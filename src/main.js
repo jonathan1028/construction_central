c7e8429a9698fde4e9f77a630a545e8fc2f99a72
jonathan1028 <lee.jonathanw@gmail.com>
2019-08-22 17:00:14 -0500

    Added server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 Vue.config.productionTip = false
 
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  uri: 'https://eu1.prisma.sh/jonathan-75b87e/construction_central/dev',
+})
+
+// End point for WSS: wss://eu1.prisma.sh/jonathan-75b87e/construction_central/dev
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
+  // inject apolloProvider here like vue-router or vuex
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
