582c343164670c50efc5508a0b32febb232f06a0
jonathan1028 <lee.jonathanw@gmail.com>
2019-08-25 00:08:26 -0500

    Fix home page header

--- src/components/public/PublicLayout.vue
+++ src/components/public/PublicLayout.vue
@@ -29,49 +29,50 @@ export default {
 <style lang="scss" scoped>
 .layout{
   display: grid;
-  grid-template-rows: 7vh 86vh 7vh;
+  grid-template-rows: 8vh 86vh 6vh;
   grid-template-areas: 
   "header"
   "content"
   "footer";
-}
-.header{
-  grid-area: header;
-  display: grid;
-  grid-template-columns: 40% 10% 50%;
-  grid-template-areas:
-  "logo navigation login";
-  padding: 1vh 1vw;
-  .logo{
-    grid-area: logo;
-    font-size: 3vmax;
-    align-self: center;
+  .header{
+    grid-area: header;
+    display: grid;
+    grid-template-columns: 40% 10% 50%;
+    grid-template-areas:
+    "logo navigation login";
+    padding: 1vh 1vw;
+    .logo{
+      grid-area: logo;
+      font-size: 4vh;
+      align-self: center;
+    }
+    .navigation{
+      grid-area: navigation;
+      align-self: center;
+    }
+    .login{
+      grid-area: login;
+      justify-self: end;
+      align-self: center;
+    }
   }
-  .navigation{
-    grid-area: navigation;
-    align-self: center;
+  .content{
+    // padding: 1vh 5vw;
   }
-  .login{
-    grid-area: login;
-    justify-self: end;
-    align-self: center;
+  .footer {
+    grid-area: footer;
+    background-color: var(--color3);
+  }
+  .gradient {
+    color: white;
+    background: -moz-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* ff3.6+ */
+    background: -webkit-gradient(linear, left top, right bottom, color-stop(0%, rgba(45,48,245,1)), color-stop(100%, rgba(35,150,153,1))); /* safari4+,chrome */
+    background: -webkit-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* safari5.1+,chrome10+ */
+    background: -o-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* opera 11.10+ */
+    background: -ms-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* ie10+ */
+    background: linear-gradient(125deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* w3c */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2D30F5', endColorstr='#239699',GradientType=1 ); /* ie6-9 */
   }
 }
-.content{
-  // padding: 1vh 5vw;
-}
-.footer {
-  grid-area: footer;
-  background-color: var(--color3);
-}
-.gradient {
-  color: white;
-  background: -moz-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* ff3.6+ */
-  background: -webkit-gradient(linear, left top, right bottom, color-stop(0%, rgba(45,48,245,1)), color-stop(100%, rgba(35,150,153,1))); /* safari4+,chrome */
-  background: -webkit-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* safari5.1+,chrome10+ */
-  background: -o-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* opera 11.10+ */
-  background: -ms-linear-gradient(325deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* ie10+ */
-  background: linear-gradient(125deg, rgba(45,48,245,1) 0%, rgba(35,150,153,1) 100%); /* w3c */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2D30F5', endColorstr='#239699',GradientType=1 ); /* ie6-9 */
-}
+
 </style>
